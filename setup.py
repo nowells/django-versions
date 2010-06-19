97d9ed3a34c3a316d3a146cae1aee936adbc6b68
Nowell Strite <nowell@strite.org>
2010-06-19 16:39:45 -0400

    Incuded tests in installed packages.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Nowell Strite',
     author_email='nowell@strite.org',
     url='http://github.com/nowells/django-versions/',
-    packages=['versions'],
+    packages=['versions', 'versions.tests'],
     license='MIT',
     install_requires=['mercurial>=1.5.2'],
     zip_safe=False,
