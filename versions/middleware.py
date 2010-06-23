f0259887539e0a9c5ede75a1baeee0469c516fcf
Nowell Strite <nowell@strite.org>
2010-06-22 22:01:31 -0400

    Updated middleware to automatically pull the active user for the request.

--- versions/middleware.py
+++ versions/middleware.py
@@ -3,6 +3,11 @@ from versions.repo import Versions
 class VersionsMiddleware(Versions):
     def process_request(self, request):
         self.start()
+        if hasattr(request, 'user') and hasattr(request.user, 'get_full_name') and hasattr(request.user, 'email') and hasattr(request.user, 'username'):
+            user_text = request.user.get_full_name() and request.user.get_full_name() or request.user.username
+            if request.user.email:
+                user_text = '%s <%s>' % (user_text, request.user.email)
+            self.set_user(user_text)
 
     def process_exception(self, request, exception):
         self.finish(exception=True)
