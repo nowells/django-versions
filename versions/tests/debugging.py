d7d09d194f6a58a75a19005c4fdbdf6d661ff271
Nowell Strite <nowell@strite.org>
2010-08-19 11:12:56 -0400

    Added ability to run multiple iterations of the test suite and aggregate all performance stats into one report.

--- versions/tests/debugging.py
+++ versions/tests/debugging.py
@@ -4,22 +4,27 @@ import pstats
 import os
 
 class Stats(object):
+    def __init__(self):
+        self.files = []
+        if not os.path.exists('profiling_data/'):
+            os.makedirs('profiling_data/')
+
     def profile(self, func):
         def _(*args, **kwargs):
-            filename = '%s-%s.stats' % (func.__name__, uuid.uuid4())
+            filename = 'profiling_data/%s-%s.stats' % (func.__name__, uuid.uuid4())
             profile.runctx('func(*args, **kwargs)', {}, {'func': func, 'args': args, 'kwargs': kwargs}, filename=filename)
-            if not hasattr(self, '_stats'):
-                self._stats = pstats.Stats(filename)
-            else:
-                self._stats.add(filename)
-            os.unlink(filename)
+            self.files.append(filename)
         return _
 
     def results(self):
-        if hasattr(self, '_stats'):
-            #self._stats.strip_dirs()
-            self._stats.sort_stats('cumulative')
-            self._stats.print_stats()
+        if self.files:
+            _stats = pstats.Stats(self.files[0])
+            os.unlink(stat_file)
+            for stat_file in self.files[1:]:
+                _stats.add(stat_file)
+                os.unlink(stat_file)
+            _stats.sort_stats('time') #cumulative
+            _stats.print_stats()
         else:
             print 'No stats were collected.'
 
